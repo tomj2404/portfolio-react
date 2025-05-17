144d251664e72ecb7af75eac1da5dc7890489937
Tomj2404 <thomasjames2404@gmail.com>
2025-05-17 19:35:35 +0530

    added resume button

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import '../css/Profile.css'
 import profilepic from '../assets/profile.jpg'
+import Button from '@mui/material/Button';
 
 const Profile = () => {
+      const onButtonClick = () => {
+        fetch("SDEResume.pdf").then((response) => {
+            response.blob().then((blob) => {
+                const fileURL =
+                    window.URL.createObjectURL(blob);
+                let alink = document.createElement("a");
+                alink.href = fileURL;
+                alink.download = "SDEResume.pdf";
+                alink.click();
+            });
+        });
+    };
   return (
     <div className='profile'>
       <img src={profilepic} alt="Profile image" />
-      <h1><span>I'm Thomas James</span>, a Front End Developer.</h1>
-      <p1>I have 3.6 years experience</p1>
+      <h1>I'm <span>Thomas James</span><br/>a Front End Developer</h1>
+      <p>I have over 4 years experience</p>
       <div className="profile-action">
-        <div className="profile-connect">Connect With Me</div>
-        <div className="profile-resume">My Resume</div>
+        <Button onClick={onButtonClick} variant="contained">My Resume</Button>
       </div>
     </div>
   )
